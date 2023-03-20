f51a70ec36a2d7ed6a7d8b0665c41b31ef225ad1
Andreas Pedersen <31003640+AndreasP-92@users.noreply.github.com>
2023-03-20 16:42:26 +0100

    d

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ app.use(controller)
 const PORT = 3000;
 
 app.listen(PORT, ()=>{console.log(`server are listening on port ${PORT}`)})
+
+// TODO:
+// Database
+// Documentation
+// Hosting
+// Fine Tuning
+// - Send, recieve and handle all data
+// - Move related functions to the service class
